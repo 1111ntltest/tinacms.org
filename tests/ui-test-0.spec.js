59cf041a984fc6dbaa2bc503a08e84db348e5ab6
1111ntltest <73410173+1111ntltest@users.noreply.github.com>
2022-01-18 19:37:54 +0100

    add playwright test files

---
+++ tests/ui-test-0.spec.js
@@ -0,0 +1,13 @@
+// example.spec.js
+const { test, expect } = require('@playwright/test');
+
+test('my test', async ({ page }) => {
+  await page.goto('https://playwright.dev/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+
+  // Expect an attribute "to be strictly equal" to the value.
+  await expect(page.locator('text=Get Started').first()).toHaveAttribute('href', '/docs/intro');
+
+});
