7a280163c910cd90f877e90b8eae8f777d35ee1d
1111ntltest <73410173+1111ntltest@users.noreply.github.com>
2022-01-24 14:49:59 +0100

    add github workflow

---
+++ .github/actions/new-action-1/action.js
@@ -0,0 +1,21 @@
+[object Response]// most @actions toolkit packages have async methods
+async function run() {
+  try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}run();
+    
+    })();
+    
+    module.exports = __webpack_exports__;
+    /******/ })()
+    ;
